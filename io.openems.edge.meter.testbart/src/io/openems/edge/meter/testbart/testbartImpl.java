f29f9a816b63e1ff55fe3e6e4fb822c18e5ba557
hornbartho <hornbartho@gmail.com>
2022-08-14 11:27:52 +0200

    Testing

---
+++ io.openems.edge.meter.testbart/src/io/openems/edge/meter/testbart/testbartImpl.java
@@ -0,0 +1,89 @@
+package io.openems.edge.meter.testbart;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.meter.api.MeterType;
+import io.openems.edge.meter.api.SymmetricMeter;
+
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "Meter.TestBart", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+public class testbartImpl extends AbstractOpenemsModbusComponent implements testbart, SymmetricMeter, ModbusComponent, OpenemsComponent {
+
+	private Config config = null;
+
+	public testbartImpl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ModbusComponent.ChannelId.values(), //
+				testbart.ChannelId.values(), //
+				SymmetricMeter.ChannelId.values()
+		);
+	}
+
+	@Reference
+	protected ConfigurationAdmin cm;
+
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	@Activate
+	void activate(ComponentContext context, Config config) throws OpenemsException {
+		if(super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm, "Modbus",
+				config.modbus_id())) {
+			return;
+		}
+		this.config = config;
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
+		return new ModbusProtocol(this, //
+				new FC3ReadRegistersTask(1000, Priority.HIGH,
+						m(SymmetricMeter.ChannelId.ACTIVE_POWER, new SignedWordElement(1000))),
+				new FC3ReadRegistersTask(1001, Priority.HIGH,
+						m(SymmetricMeter.ChannelId.CURRENT, new SignedWordElement(1001)))
+				);
+	}
+
+	@Override
+	public String debugLog() {
+		return "L:" + this.getActivePower().asString();
+	}
+
+	@Override
+	public MeterType getMeterType() {
+		// TODO Auto-generated method stub
+		return this.config.type();
+	}
+}
